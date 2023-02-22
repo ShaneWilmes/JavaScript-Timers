14abcc32a006a0c64c7aac32f7dbcef9030a0c46
ShaneWilmes <shanewilmes@yahoo.com>
2023-02-21 17:02:10 -0700

    first conditional

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,10 @@ function countdown() {
 
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
-    
+    if(timeLeft > 1) {
+        timerEl.textContent = timeLeft + "seconds remaining";
+        timeLeft--;
+    }
   });
 }
 
